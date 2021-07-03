d41a9534b0aca06e7005f829f9deb0daeb5327a9
RodaDev <RodaDev@yahoo.com>
2021-07-03 17:30:53 +0300

    Create xcode project

---
+++ RSSNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RSSNews
+//
+//  Created by RodaDev on 03.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
